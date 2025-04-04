024a2a8a99952a56f454360f5e800dce8c695aee
Richard RAKOTOMAVO <darirarakotomavo@example.com>
2025-04-04 17:52:47 +0300

    07_création-best-selling-slider

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,68 +17,80 @@ export default async function HomePage() {
     tag: "new-arrival",
     limit: 4,
   });
-  const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
+ const bestSellingProducts = await getProductsByTag({ tag: "best-seller" });
 
-  const featureds = await getProductsForCard({
-    tag: "featured",
-    limit: 4,
-  });
-  const bestSellers = await getProductsForCard({
-    tag: "best-seller",
-    limit: 4,
-  });
+ const todaysDeals = await getProductsByTag({ tag: "todays-deal" });
 
-  const cards = [
-    {
-      title: "Categories to explore",
-      link: {
-        text: "See More",
-        href: "/search",
-      },
-      items: categories.map((category) => ({
-        name: category,
-        image: `/images/${toSlug(category)}.jpg`,
-        href: `/search?category=${category}`,
-      })),
-    },
-    {
-      title: "Explore New Arrivals",
-      items: newArrivals,
-      link: {
-        text: "View All",
-        href: "/search?tag=new-arrival",
-      },
-    },
-    {
-      title: "Discover Best Sellers",
-      items: bestSellers,
-      link: {
-        text: "View All",
-        href: "/search?tag=best-seller",
-      },
-    },
-    {
-      title: "Featured Products",
-      items: featureds,
-      link: {
-        text: "Shop Now",
-        href: "/search?tag=featured",
-      },
-    },
-  ];
+ const featureds = await getProductsForCard({
+   tag: "featured",
+   limit: 4,
+ });
+ const bestSellers = await getProductsForCard({
+   tag: "best-seller",
+   limit: 4,
+ });
 
-  return (
-    <>
-      <HomeCarousel items={data.carousels} />
-      <div className="md:p-4 md:space-y-4 bg-border">
-        <HomeCard cards={cards} />
-        <Card className="w-full rounded-none">
-          <CardContent className="p-4 items-center gap-3">
-            <ProductSlider title={"Today's Deals"} products={todaysDeals} />
-          </CardContent>
-        </Card>
-      </div>
-    </>
-  );
+ const cards = [
+   {
+     title: "Categories to explore",
+     link: {
+       text: "See More",
+       href: "/search",
+     },
+     items: categories.map((category) => ({
+       name: category,
+       image: `/images/${toSlug(category)}.jpg`,
+       href: `/search?category=${category}`,
+     })),
+   },
+   {
+     title: "Explore New Arrivals",
+     items: newArrivals,
+     link: {
+       text: "View All",
+       href: "/search?tag=new-arrival",
+     },
+   },
+   {
+     title: "Discover Best Sellers",
+     items: bestSellers,
+     link: {
+       text: "View All",
+       href: "/search?tag=best-seller",
+     },
+   },
+   {
+     title: "Featured Products",
+     items: featureds,
+     link: {
+       text: "Shop Now",
+       href: "/search?tag=featured",
+     },
+   },
+ ];
+
+ return (
+   <>
+     <HomeCarousel items={data.carousels} />
+     <div className="md:p-4 md:space-y-4 bg-border">
+       <HomeCard cards={cards} />
+
+       <Card className="w-full rounded-none">
+         <CardContent className="p-4 items-center gap-3">
+           <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+         </CardContent>
+       </Card>
+       <Card className="w-full rounded-none">
+         <CardContent className="p-4 items-center gap-3">
+           <ProductSlider
+             title={"Best Selling Products"}
+             products={bestSellingProducts}
+             hideDetails
+           />
+         </CardContent>
+       </Card>
+     </div>
+   </>
+ );
 }
     
