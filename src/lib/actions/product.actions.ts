e764c0487921b3e763e7c8c859b65ed0f85a7089
Richard RAKOTOMAVO <darirarakotomavo@example.com>
2025-04-04 17:35:16 +0300

    06-création-todays-deals-slider

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -4,7 +4,7 @@
 //import Product from "@/lib/db/models/product.model";
 import { connectToDatabase } from "../db";
 import Product from "../db/models/product.model";   
-
+import { IProduct } from "@/lib/db/models/product.model";
 export async function getAllCategories() {
   await connectToDatabase();
   const categories = await Product.find({ isPublished: true }).distinct(
@@ -35,4 +35,22 @@ export async function getProductsForCard({
     href: string;
     image: string;
   }[];
+}
+ 
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+  tag,
+  limit = 10,
+}: {
+  tag: string;
+  limit?: number;
+}) {
+  await connectToDatabase();
+  const products = await Product.find({
+    tags: { $in: [tag] },
+    isPublished: true,
+  })
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
 }
